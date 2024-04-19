b3a6305947259cd30cf76c1e094a4a92986af3ba
Underscore-Krzy <krzyzaczek101@gmail.com>
2024-04-19 14:29:28 +0200

    initial commit

--- src/main/java/com/example/hackaton/HelloApplication.java
+++ src/main/java/com/example/hackaton/HelloApplication.java
@@ -1,17 +1,19 @@
 package com.example.hackaton;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    public static Pane root = new Pane();
+    public static final int WIDTH = 1280;
+    public static final int HEIGHT = 720;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
