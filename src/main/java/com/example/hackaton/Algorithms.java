65370c3d1353f5e7eb3066647bfd6b0db0feb03b
Ada <adakruc@gmail.com>
2024-04-20 05:15:42 +0200

    feat: weights

--- src/main/java/com/example/hackaton/Algorithms.java
+++ src/main/java/com/example/hackaton/Algorithms.java
@@ -9,22 +9,22 @@ public class Algorithms {
     public static double calculateSimilarity(Animal idealAnimal, Animal currentAnimal) {
         if (!idealAnimal.fur && currentAnimal.fur) return 0;
         double similarity = 0;
-        similarity += 1*Math.pow(idealAnimal.careTimeNeeded - currentAnimal.careTimeNeeded, 2);
-        similarity += 1*Math.pow(idealAnimal.resourcefulness - currentAnimal.resourcefulness, 2);
-        similarity += 1*Math.pow(idealAnimal.competentWithAnimals - currentAnimal.competentWithAnimals, 2);
-        similarity += 1*Math.pow(idealAnimal.impulsiveness - currentAnimal.impulsiveness, 2);
-        similarity += 1*Math.pow(idealAnimal.income - currentAnimal.income, 2);
-        similarity += 1*Math.pow(idealAnimal.sex - currentAnimal.sex, 2);
-        similarity += 1*Math.pow(idealAnimal.gardenSize - currentAnimal.gardenSize, 2);
-        similarity += 1*Math.pow(idealAnimal.freeTime - currentAnimal.freeTime, 2);
-        similarity += 1*Math.pow(idealAnimal.activeLifestyle - currentAnimal.activeLifestyle, 2);
-        similarity += 1*Math.pow(idealAnimal.livingArea - currentAnimal.livingArea, 2);
-        similarity += 1*Math.pow(idealAnimal.currentAnimals - currentAnimal.currentAnimals, 2);
-        similarity += 1*Math.pow(idealAnimal.houseType - currentAnimal.houseType, 2);
-        similarity += 1*Math.pow(idealAnimal.housemateCount - currentAnimal.housemateCount, 2);
-        similarity += 1*Math.pow(idealAnimal.qustionareeAge - currentAnimal.qustionareeAge, 2);
-        similarity += 1*Math.pow(idealAnimal.children - currentAnimal.children, 2);
-        similarity += 1*Math.pow(idealAnimal.animalsActivity - currentAnimal.animalsActivity, 2);
+        similarity += Math.pow(idealAnimal.careTimeNeeded - currentAnimal.careTimeNeeded, 2)*0.7;
+        similarity += Math.pow(idealAnimal.resourcefulness - currentAnimal.resourcefulness, 2)*0.3;
+        similarity += Math.pow(idealAnimal.competentWithAnimals - currentAnimal.competentWithAnimals, 2)*0.5;
+        similarity += Math.pow(idealAnimal.impulsiveness - currentAnimal.impulsiveness, 2)*0.5;
+        similarity += Math.pow(idealAnimal.income - currentAnimal.income, 2)*0.4;
+        similarity += Math.pow(idealAnimal.sex - currentAnimal.sex, 2);
+        similarity += Math.pow(idealAnimal.gardenSize - currentAnimal.gardenSize, 2)*0.4;
+        similarity += Math.pow(idealAnimal.freeTime - currentAnimal.freeTime, 2)*0.7;
+        similarity += Math.pow(idealAnimal.activeLifestyle - currentAnimal.activeLifestyle, 2)*0.8;
+        similarity += Math.pow(idealAnimal.livingArea - currentAnimal.livingArea, 2)*0.6;
+        similarity += Math.pow(idealAnimal.currentAnimals - currentAnimal.currentAnimals, 2)*0.6;
+        similarity += Math.pow(idealAnimal.houseType - currentAnimal.houseType, 2)*0.7;
+        similarity += Math.pow(idealAnimal.housemateCount - currentAnimal.housemateCount, 2)*0.5;
+        similarity += Math.pow(idealAnimal.qustionareeAge - currentAnimal.qustionareeAge, 2)*0.5;
+        similarity += Math.pow(idealAnimal.children - currentAnimal.children, 2)*0.4;
+        similarity += Math.pow(idealAnimal.animalsActivity - currentAnimal.animalsActivity, 2)*0.7;
         similarity += 2*calculateTraitsSimilarity(idealAnimal.ownerTraits, currentAnimal.ownerTraits);
         return Math.sqrt(similarity);
     }
